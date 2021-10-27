1a78a40089a90683b4aa71cf31066fc244bcfcf6
Harison Gachuru <harisongachuru@gmail.com>
2021-10-28 02:17:55 +0300

    Update foreign key references

--- scripts/migrations.sql
+++ scripts/migrations.sql
@@ -3,6 +3,10 @@ ALTER TABLE IF EXISTS accounts_customuser RENAME TO accounts_user;
 ALTER TABLE IF EXISTS accounts_customuser_groups RENAME TO accounts_user_groups;
 ALTER TABLE IF EXISTS accounts_customuser_user_permissions RENAME TO accounts_user_user_permissions;
 
+-- rename columns
+ALTER TABLE IF EXISTS accounts_user_groups RENAME COLUMN customuser_id TO user_id;
+ALTER TABLE IF EXISTS accounts_user_user_permissions RENAME COLUMN customuser_id TO user_id;
+
 -- rename sequences
 ALTER SEQUENCE IF EXISTS accounts_customuser_id_seq RENAME TO accounts_user_id_seq;
 ALTER SEQUENCE IF EXISTS accounts_customuser_groups_id_seq RENAME TO accounts_user_groups_id_seq;
